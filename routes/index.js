e0ba0c883035a220376334e566731b361ad23582
Jeff DeWitt <jdewitt@adinternal.cutlerhomes.com>
2020-04-20 15:35:52 -0400

    Changed style of Cards on index page.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Title One Agency' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Express' });
+  res.render('services', { title: 'Services | Title One Agency' });
 });
 
 module.exports = router;
